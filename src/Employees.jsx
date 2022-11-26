8fe0b9487e1b147f85efa9b072858ffc6d8a2f6e
Mwenje <mwenje24@gmail.com>
2022-11-26 15:36:35 +0300

    added an image to member card

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -3,6 +3,7 @@ import nick from './images/nick2.png'
 import judy from './images/judy.png'
 
 function Employees() {
+    const [selectedteam, setSelectedTeam] = useState('TeamA');
     const [employees, setEmployees] = useState([
         {
             id:1,
@@ -41,12 +42,35 @@ function Employees() {
         },
 
     ])
+    function handleEmployeeCardClick(e){
+        const transformEmployee = employees.map((employee) => 
+        employee.id === parseInt(e.currentTarget.id)?
+        ((employee.team === selectedteam)?
+        {...employee, team:''}:{...employee, team:selectedteam}):employee
+        );
+        setEmployees(transformEmployee);
+    }
   return (
     <div className='container'>
         <div className="row justify-content-center">
+            <div className="col-6">
+                <select className='form-select form-select-sm' 
+                    value={selectedteam} 
+                    onChange={(e) => setSelectedTeam(e.target.value)}>
+                    <option value='TeamA'>TeamA</option>
+                    <option value='TeamB'>TeamB</option>
+                    <option value='TeamC'>TeamC</option>
+                </select>
+            </div>
+        </div>
+        <div className="row justify-content-center">
             {
-                employees.map((employee) =>(
-                    <div style={{cursor:'pointer'}} id={employee.id} className='col-3 m-1 border shadow'>
+                employees.map((employee, index) =>(
+                    <div style={{cursor:'pointer'}} 
+                        key={index}
+                        id={employee.id} 
+                        onClick={handleEmployeeCardClick}
+                        className={(employee.team === selectedteam ? 'col-3 m-1 border shadow':'col-3 m-1 border shadow')}>
                         {/* style={{width:'100px', height:'auto'}} */}
                         <img className='card-img-top' src={employee.gender === 'male' ? nick : judy} alt="." />
                         <div className="card-body">
